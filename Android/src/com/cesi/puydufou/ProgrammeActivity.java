efbe2d05d4a495bb2cb768f3559b24833aefb8e5
Bigdool <rodolphe.fauquez@gmail.com>
2015-06-15 01:35:30 +0200

    Archi de base
    Les 3 activity de base avec leur layout. Projet de départ

---
+++ Android/src/com/cesi/puydufou/ProgrammeActivity.java
@@ -0,0 +1,17 @@
+package com.cesi.puydufou;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Bigdool on 15/06/2015.
+ */
+public class ProgrammeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_programme);
+
+    }
+}
